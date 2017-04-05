1fd2f913cd5c0a5bcc47fe9e28ff563b17382969
evtr <evtr@nextstepcitizen.com>
2017-04-05 22:52:22 +0200

    en app mere og hermed SSO

--- consent-development/consentidp/consentidp-metadata/saml20-sp-remote.php
+++ consent-development/consentidp/consentidp-metadata/saml20-sp-remote.php
@@ -96,3 +96,92 @@ jvrhFkdUxac94tIYKQ==',
   'validate.authnrequest' => true,
   'saml20.sign.assertion' => true,
 );
+
+$metadata['dev:kit:appb'] = array (
+  'entityid' => 'dev:kit:appb',
+  'contacts' => 
+  array (
+  ),
+  'metadata-set' => 'saml20-sp-remote',
+  'AssertionConsumerService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+      'Location' => 'http://frontend:8084/appb/saml/SSO',
+      'index' => 0,
+      'isDefault' => true,
+    ),
+    1 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
+      'Location' => 'http://frontend:8084/appb/saml/SSO',
+      'index' => 1,
+    ),
+  ),
+  'SingleLogoutService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+      'Location' => 'http://frontend:8084/appb/saml/SingleLogout',
+    ),
+    1 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'http://frontend:8084/appb/saml/SingleLogout',
+    ),
+  ),
+  'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+  'keys' => 
+  array (
+    0 => 
+    array (
+      'encryption' => false,
+      'signing' => true,
+      'type' => 'X509Certificate',
+      'X509Certificate' => 'MIIDmTCCAoGgAwIBAgIJAKZHOfiTvGPuMA0GCSqGSIb3DQEBCwUAMGMxCzAJBgNVBAYTAkRLMRMw
+EQYDVQQIDApTb21lLVN0YXRlMRQwEgYDVQQKDAtLdmFsaXRldHNJVDEZMBcGA1UECwwQQXBwIEIg
+YWZkZWxpbmdlbjEOMAwGA1UEAwwFQXBwIEIwHhcNMTcwNDA1MDkwODM2WhcNMjcwNDA1MDkwODM2
+WjBjMQswCQYDVQQGEwJESzETMBEGA1UECAwKU29tZS1TdGF0ZTEUMBIGA1UECgwLS3ZhbGl0ZXRz
+SVQxGTAXBgNVBAsMEEFwcCBCIGFmZGVsaW5nZW4xDjAMBgNVBAMMBUFwcCBCMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxg7eYVuHxYGh6dZJ91jTbkDn2QDrZIkwV7s1Kqi+9/LCFvEX
+vtaJr0DNtB/U9RIO7uWZevBBBEcOqrwyKbrJf5Iq2pb1GQX2QfO/p1TiySpIQLc8iajtn64Iad1D
+zMMBOJVvm8Ngu6B5j/Xi/suxqPEj2DtSLg3+M4fofU/GXnlWdmI1FxmIU08cZqTNKaFna0XFHL/r
+bIZxibW99euFmsRvhzxu8AOzorp0cCnWiTq0lUj4RPBgngWra2IbDuEY7e2Kb6Q+mPTPv3/KYRpX
+ppKfDSysDUlgu/PrvnrsJND1gUf51sDvLvjE2jDwkFT50+RQlPwb8uDgyyOzmRaiKQIDAQABo1Aw
+TjAdBgNVHQ4EFgQUtJftZziO5xQIUI3wwVO1kJKY03QwHwYDVR0jBBgwFoAUtJftZziO5xQIUI3w
+wVO1kJKY03QwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAvf/qXPlNaPG9IZId4+ic
+aI6NVsZWi9JKT6QEULPQGmNRhLp+O+oPPAdAHSrFIm4DVRxhO1EjBuoYztwkWGPIceLLSTHBt3gH
+/lS7yK2F6NaB6ihH6x0Fu8Ais1iYdaMCg5HjT3QoA8YZWk2mGFR68GC/c9NCV+HGGGiwYEZq4SDw
+FHijlDz0QL1XAKxZorAY53wrE5UPq+fc0nh5ajkryxi+GelyMunmk/gedzoE1J4eNjdJXK0dobSq
+VdEQoomFZYfikwygp4ljGrTTPL9OsPj0LGL6HS3CU5bLe1tG3UXRVLav5uI//p67MgY52tKodCHZ
+1i3XAfJsgdjNEe46nQ==',
+    ),
+    1 => 
+    array (
+      'encryption' => true,
+      'signing' => false,
+      'type' => 'X509Certificate',
+      'X509Certificate' => 'MIIDmTCCAoGgAwIBAgIJAKZHOfiTvGPuMA0GCSqGSIb3DQEBCwUAMGMxCzAJBgNVBAYTAkRLMRMw
+EQYDVQQIDApTb21lLVN0YXRlMRQwEgYDVQQKDAtLdmFsaXRldHNJVDEZMBcGA1UECwwQQXBwIEIg
+YWZkZWxpbmdlbjEOMAwGA1UEAwwFQXBwIEIwHhcNMTcwNDA1MDkwODM2WhcNMjcwNDA1MDkwODM2
+WjBjMQswCQYDVQQGEwJESzETMBEGA1UECAwKU29tZS1TdGF0ZTEUMBIGA1UECgwLS3ZhbGl0ZXRz
+SVQxGTAXBgNVBAsMEEFwcCBCIGFmZGVsaW5nZW4xDjAMBgNVBAMMBUFwcCBCMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxg7eYVuHxYGh6dZJ91jTbkDn2QDrZIkwV7s1Kqi+9/LCFvEX
+vtaJr0DNtB/U9RIO7uWZevBBBEcOqrwyKbrJf5Iq2pb1GQX2QfO/p1TiySpIQLc8iajtn64Iad1D
+zMMBOJVvm8Ngu6B5j/Xi/suxqPEj2DtSLg3+M4fofU/GXnlWdmI1FxmIU08cZqTNKaFna0XFHL/r
+bIZxibW99euFmsRvhzxu8AOzorp0cCnWiTq0lUj4RPBgngWra2IbDuEY7e2Kb6Q+mPTPv3/KYRpX
+ppKfDSysDUlgu/PrvnrsJND1gUf51sDvLvjE2jDwkFT50+RQlPwb8uDgyyOzmRaiKQIDAQABo1Aw
+TjAdBgNVHQ4EFgQUtJftZziO5xQIUI3wwVO1kJKY03QwHwYDVR0jBBgwFoAUtJftZziO5xQIUI3w
+wVO1kJKY03QwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAvf/qXPlNaPG9IZId4+ic
+aI6NVsZWi9JKT6QEULPQGmNRhLp+O+oPPAdAHSrFIm4DVRxhO1EjBuoYztwkWGPIceLLSTHBt3gH
+/lS7yK2F6NaB6ihH6x0Fu8Ais1iYdaMCg5HjT3QoA8YZWk2mGFR68GC/c9NCV+HGGGiwYEZq4SDw
+FHijlDz0QL1XAKxZorAY53wrE5UPq+fc0nh5ajkryxi+GelyMunmk/gedzoE1J4eNjdJXK0dobSq
+VdEQoomFZYfikwygp4ljGrTTPL9OsPj0LGL6HS3CU5bLe1tG3UXRVLav5uI//p67MgY52tKodCHZ
+1i3XAfJsgdjNEe46nQ==',
+    ),
+  ),
+  'validate.authnrequest' => true,
+  'saml20.sign.assertion' => true,
+);
