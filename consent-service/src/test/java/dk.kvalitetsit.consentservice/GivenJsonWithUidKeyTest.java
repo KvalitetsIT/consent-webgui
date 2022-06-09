9489fb7076351b1c72f1f318e9a22189dd9708b7
Henrik Eidnes Nielsen <hen@kvalitetsit.dk>
2022-06-09 11:08:21 +0200

    Fix compile error

--- consent-service/src/test/java/dk.kvalitetsit.consentservice/GivenJsonWithUidKeyTest.java
+++ consent-service/src/test/java/dk.kvalitetsit.consentservice/GivenJsonWithUidKeyTest.java
@@ -1,5 +1,6 @@
 package dk.kvalitetsit.consentservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.kvalitetsit.consentservice.controller.ConsentController;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.junit.jupiter.api.Assertions;
@@ -14,7 +15,7 @@ public class GivenJsonWithUidKeyTest {
         int expected = 123456;
         String uidKey = "cprnumber";
         String testJson = "{\"UserAttributes\": {" + "\n" + "  " + "\"" + uidKey + "\": [\n" + "\"" + expected + "\"\n]}\n}";
-        String uid = new ConsentController().getUid(uidKey, Base64.encodeBase64String(testJson.getBytes()));
+        String uid = new ConsentController().getUid(new ObjectMapper(), uidKey, Base64.encodeBase64String(testJson.getBytes()));
         Assertions.assertEquals(Integer.toString(expected), uid);
     }
 }
